418a2c051976e7ee21b7b2fc887af8d8e4a7a4c2
lufherna.V2 <lufherna.v2@gmail.com>
2024-04-22 19:39:48 -0400

    Added env variable for open API key

--- codecept.conf.js
+++ codecept.conf.js
@@ -33,22 +33,22 @@ exports.config = {
       enabled: true
     }
   },
-  // ai: {
-  //   request: async (messages) => {
-  //     const OpenAI = require('openai');
-  //     const openai = new OpenAI({apiKey: process.env[]})
-  //     const response = await openai.chat.completions.create({
-  //       model: 'gpt-3.5-turbo-0125',
-  //       messages,
-  //     });
-  //     return response?.data?.choices[0]?.message?.content;
-  //   }
-  // },
   ai: {
-    request: async(messages) => {
-      const ai = require('openai');
-      return ai.send(messages);
+    request: async (messages) => {
+      const OpenAI = require('openai');
+      const openai = new OpenAI({apiKey: process.env['OPENAI_API_KEY']})
+      const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo-0125',
+        messages,
+      });
+      return response?.data?.choices[0]?.message?.content;
     }
   },
+  // ai: {
+  //   request: async(messages) => {
+  //     const ai = require('openai');
+  //     return ai.send(messages);
+  //   }
+  // },
   name: 'CodeceptPlaywright'
 }
